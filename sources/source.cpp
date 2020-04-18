7dbacc7f01fd8e91c7e9a95225db2b732e64b34e
dimakirol <60985893+dimakirol@users.noreply.github.com>
2020-04-18 17:39:37 +0300

    18.04.2020 17:39 ko

--- sources/source.cpp
+++ sources/source.cpp
@@ -14,19 +14,6 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/beast.hpp>
 
-#include <boost/program_options/cmdline.hpp>
-#include <boost/program_options/config.hpp>
-#include <boost/program_options/environment_iterator.hpp>
-#include <boost/program_options/eof_iterator.hpp>
-#include <boost/program_options/errors.hpp>
-#include <boost/program_options/option.hpp>
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/parsers.hpp>
-#include <boost/program_options/positional_options.hpp>
-#include <boost/program_options/value_semantic.hpp>
-#include <boost/program_options/variables_map.hpp>
-#include <boost/program_options/version.hpp>
-
 namespace po=boost::program_options;
 
 struct _Params{
